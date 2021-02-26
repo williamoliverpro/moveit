c35ff6d8065a6463880c22af2bd8ffb4cd7fe327
William Oliveira <williamoliverpro@gmail.com>
2021-02-26 05:06:38 -0300

    context api, components, notification added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { Countdown } from '../components/Countdown'
-import Head from 'next/head'
+import { ChallengeBox } from '../components/ChallengeBox'
 
 import styles from '../styles/pages/Home.module.css'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 export default function Home() {
   return (
@@ -16,17 +19,19 @@ export default function Home() {
       </Head>
 
       <ExperienceBar />
-
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-
-        </div>
-      </section>
+      
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
